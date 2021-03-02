821df04ee6248ffdd9c2550372eea96fd9f5d236
小行星 <376052450@qq.com>
2021-03-02 23:41:22 +0800

    feat: commit java method and field builder code

--- src/main/java/org/burgeon/turtle/core/model/source/JavaType.java
+++ src/main/java/org/burgeon/turtle/core/model/source/JavaType.java
@@ -27,6 +27,14 @@ public class JavaType {
 
     private boolean isChar;
 
+    /**
+     * Java基础类型：byte、short、int、long、float、double、boolean、char
+     */
+    private boolean isPrimitive;
+
+    /**
+     * 类型为Object，则可以转换成JavaClass
+     */
     private boolean isObject;
 
     private boolean isArray;
@@ -36,5 +44,7 @@ public class JavaType {
     private boolean isList;
 
     private boolean isMap;
+
+    private boolean isVoid;
 
 }

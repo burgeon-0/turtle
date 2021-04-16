beed655013f8d8f7f70b39da7cea2050dab6d03c
Sam Lu <luxiaocong524@163.com>
2021-04-16 19:46:46 +0800

    fix: export api docs by idea plugin

--- src/main/java/org/burgeon/turtle/plugin/idea/action/ApiBlueprintExportAction.java
+++ src/main/java/org/burgeon/turtle/plugin/idea/action/ApiBlueprintExportAction.java
@@ -3,11 +3,17 @@ package org.burgeon.turtle.plugin.idea.action;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.burgeon.turtle.core.common.Constants;
 import org.burgeon.turtle.core.process.DefaultProcessor;
 import org.burgeon.turtle.core.process.Notifier;
 import org.burgeon.turtle.core.process.Processor;
+import org.burgeon.turtle.core.utils.EnvUtils;
 import org.burgeon.turtle.export.DefaultExporterConfig;
+import org.burgeon.turtle.plugin.idea.ConfigInitializer;
+import org.burgeon.turtle.plugin.idea.NotificationHelper;
 import org.burgeon.turtle.plugin.idea.notifier.IdeaApiBlueprintNotifier;
+
+import java.util.concurrent.*;
 
 /**
  * 菜单[导出API Blueprint文档]
@@ -29,6 +35,7 @@ public class ApiBlueprintExportAction extends AnAction {
     @Override
     public void update(AnActionEvent e) {
         if (!init) {
+            ConfigInitializer.init(e.getProject());
             exporterConfig.init();
             init = true;
         }
@@ -43,11 +50,20 @@ public class ApiBlueprintExportAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         log.debug("Receive an [Export API Blueprint] action.");
 
-        // 进行处理
-        Processor processor = new DefaultProcessor();
-        Notifier notifier = new IdeaApiBlueprintNotifier();
-        processor.setNotifier(notifier);
-        processor.process();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>());
+        executor.submit(() -> {
+            // 进行处理
+            Processor processor = new DefaultProcessor();
+            Notifier notifier = new IdeaApiBlueprintNotifier();
+            processor.setNotifier(notifier);
+            processor.process();
+
+            String targetPath = EnvUtils.getStringProperty(Constants.TARGET_PATH);
+            NotificationHelper.info(e.getProject(), "导出成功", "API文档已导出到：" + targetPath);
+        });
+        executor.shutdown();
     }
 
 }
